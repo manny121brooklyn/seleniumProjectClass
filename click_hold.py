83e37dc8138774eeb524d6a2b54687c7c13c23fd
manny121 <manny121@gmail.com>
2023-01-21 22:40:24 -0500

    started click and hold scenario completed

--- click_hold.py
+++ click_hold.py
@@ -25,11 +25,19 @@ try:
     time.sleep(3)
 
     # Code for click and hold
+    # click on element and hold it
     element = driver.find_element(By.LINK_TEXT, name_locator)
     time.sleep(3)
     action = ActionChains(driver)
-    action.click_and_hold(element).perform()
-
+    # action.click_and_hold(element).perform()
+    # click on element and hold it, pause 5 seconds
+    # action.click_and_hold(element).pause(5).perform()
+    # click on element and hold it, pause for 5 seconds, and release it.
+    action.click_and_hold(element).pause(5).release(on_element=element)
+    time.sleep(2)
+    # verify web element clicked and held
+    print(driver.find_element(By.LINK_TEXT, name_locator).text)
+#
 
 except Exception as err:
     print('Python Exception: Test failed with the following exception.')
